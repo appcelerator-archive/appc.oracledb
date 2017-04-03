8eef396492dd89e3c10210f77bb9ba786349923d
Emil Ruzhenov <eruzhenov@axway.com>
2017-04-03 15:37:12 +0300

    [ARRSOF-151] Create unit tests for the getPrimaryKeyColumn, getTableName, getTableSchema, returnPlaceholder, translateResults, translateWhereToQuery utility methods (#27)
    * [ARRSOF-151] Unit tests getPrimaryKeyColumn.js
    
    * [ARRSOF-151] Unit tests getTableName.js
    
    * [ARRSOF-151] Unit tests getTableSchema.js
    
    * [ARRSOF-151] Unit tests returnPlaceholder.js
    
    * [ARRSOF-151] Unit tests translateResults.js
    
    * [ARRSOF-151] Unit tests translateWhereToQuery.js

---
+++ test/unit/utility/returnPlaceholder.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const tap = require('tap')
+const test = tap.test
+const sinon = require('sinon')
+const returnPlaceholder = require('../../../lib/utility/returnPlaceholder').returnPlaceholder
+
+const sandbox = sinon.sandbox
+
+tap.beforeEach((done) => {
+    sandbox.create()
+    done()
+})
+
+tap.afterEach((done) => {
+    sandbox.restore()
+    done()
+})
+
+test('### returnPlaceholder Call - OK Case ###', function (t) {
+    // Execution
+    const placeholder = returnPlaceholder('ColumnName', 7, [])
+
+    // Tests
+    t.equals(placeholder, ':ColumnName')
+
+    t.end()
+})
